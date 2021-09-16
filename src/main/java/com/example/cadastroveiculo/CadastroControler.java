153fcd2ef1c033ecae557096a003650f746cca4c
Rogerio Krychak <roger.krychak@gmail.com>
2021-09-15 22:24:16 -0300

    Initial commit

---
+++ src/main/java/com/example/cadastroveiculo/CadastroControler.java
@@ -0,0 +1,10 @@
+package com.example.cadastroveiculo;
+
+
+
+public class CadastroControler {
+    //definição do onAction
+    public void cadastro(){
+        System.out.println("Botão Cadastro selecionado!!!");
+    }
+}
